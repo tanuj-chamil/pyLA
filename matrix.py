498ce6f68c5ce63db291fd6712b462a2a547e448
Tanuj Chamil <tanujchamil@gmail.com>
2022-10-22 14:30:46 +0530

    added __add__  in matrix class

--- matrix.py
+++ matrix.py
@@ -1,4 +1,6 @@
 from ast import Raise
+from unittest import result
+from xml.dom.minidom import Element
 
 
 class Matrix:
@@ -12,26 +14,57 @@ class Matrix:
             elements : list
         """
         if type(elements)!= list or type(elements[0])!=list:
-            raise TypeError
+            raise TypeError("Matrix must be a 2D-list")
         
-        dimN = len(elements[0])
+        dimN = len(elements)
+        dimM = len(elements[0])
         for row in elements[1:]:
-            if len(row) != dimN:
-                raise ValueError
+            if len(row) != dimM:
+                raise ValueError("Inconsistent row dimensions")
 
+        self.dimN = dimN
+        self.dimM = dimM
         self.elements = elements
+    
+    def dim(self):
+        '''
+        Returns the dimension of the matrix
+                Parameters:
+                    None
+                Returns:
+                    tuple (int): The dimension of the matrix
+        '''
+        return self.dimN, self.dimM
+
 
     def __str__(self) -> str:
         '''
         Returns the string representation of the matrix
                 Parameters:
-                    self (Matrix): A Matrix Object
+                    None
                 Returns:
                     string (str): The string representation of the matrix 
         '''
-
+        
         out = []
         for row in self.elements:
             out.append("\t".join(map(str,row)))
         string = "\n".join(out)
         return string
+
+    def __add__(self,other):
+        '''
+        Returns sum of the matrix two matrices
+                Parameters:
+                    other (Matrix): A Matrix Object
+                Returns:
+                    result (Matrix): sum of the matrix two matrices
+        '''
+        if self.dim() != other.dim():
+            raise IndexError("Dimensions do not match")
+
+        result = []
+        for n in range(self.dimN):
+            result.append([self.elements[n][i]+ other.elements[n][i] for i in range(self.dimM)])
+        
+        return Matrix(result)
--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 from matrix import Matrix as mtx
 
 mtxA = mtx([[1,2,5.222],[5,6,7/2]])
+mtxB = mtx([[1,1,1.1],[1,1,1]])
 
 print(mtxA)
+print(mtxA + mtxB)
