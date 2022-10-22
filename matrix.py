2b2093aa2d9862e15a08b83d799886910ec73add
Tanuj Chamil <tanujchamil@gmail.com>
2022-10-22 12:16:39 +0530

    matrix : __init__ , __str__ added

---
+++ matrix.py
@@ -0,0 +1,27 @@
+class Matrix:
+
+    def __init__(self,elements) -> None:
+        """
+        Constructs all the necessary attributes for the Matrix object.
+
+        Parameters
+        ----------
+            elements : list
+        """
+
+        self.elements = elements
+
+    def __str__(self) -> str:
+        '''
+        Returns the string representation of the matrix
+                Parameters:
+                    self (Matrix): A Matrix Object
+                Returns:
+                    string (str): The string representation of the matrix 
+        '''
+        
+        out = []
+        for row in self.vals:
+            out.append("\t".join(map(str,row)))
+        string = "\n".join(out)
+        return string
---
+++ test.py
@@ -0,0 +1,5 @@
+from matrix import Matrix as mtx
+
+mtxA = mtx([[1,2,5.222],[5,6,5/7]])
+
+print(mtxA)
